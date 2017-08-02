1840b0d43981a1e8e04f91bd024f0c8ef15ec07c
Hien Tran <aznketchup@yahoo.com>
2017-08-01 18:14:04 -0700

    Initialize Landing page. -Imported connect from react-redux. -Defined mapStateToProps. -Connected mapStateToProps to Landing page.

---
+++ js/Landing.jsx
@@ -0,0 +1,20 @@
+/*
+mapStateToProps takes the state and only replace what need.
+connect takes in a function and returns a function. 
+*/
+
+import React from "react";
+import {connect} from "react-redux"
+import {Link} from "react-router-dom";
+
+const Landing = () => (
+	<div className="landing">
+		<h1>svideo</h1>
+		<input type="text" placeholder="Search">
+		<Link to="/search">or Browse All</Link>
+	</div>
+);
+
+const mapStateToProps = state => ({searchTerm: state.searchTerm});
+
+export default connect(mapStateToProps)(Landing);
