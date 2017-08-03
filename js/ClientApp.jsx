6982e0994679ad521d9091fbfc4ca19de7eb9eba
Hien Tran <aznketchup@yahoo.com>
2017-08-02 17:53:52 -0700

    Modify webpack.config.js -Add preset to loader's query so webpack to compile jsx file. -Link css to index.html. -Re-initalize ClientApp.jsx to start building svideo application.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () =>
+	<div className="app">
+		<div className="landing">
+			<h1>svideo</h1>
+			<input type="text" placeholder="Search" />
+			<a>or Browse All</a>
+		</div>
+	</div>;
 
-const MyTitle = props => ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+render(<App />, document.getElementById('app'));
 
-const MyFirstComponent = () =>
-  ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
+// const ce = React.createElement;
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+// const MyTitle = function(props) {
+// 	return (
+// 		<div>
+// 			<h1 style={{ color: props.color }}>
+// 				{props.title}
+// 			</h1>
+// 		</div>
+// 	);
+// };
+
+// ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+
+// const MyFirstComponent = function() {
+// 	return (
+// 		<div>
+// 			<MyTitle title="Game of Thrones" color="YellowGreen" />
+// 			<MyTitle title="Stranger Things" color="YellowGreen" />
+// 			<MyTitle title="Rick and Morty" color="peru" />
+// 			<MyTitle title="Silicon Valley" color="LimeGreen" />
+// 		</div>
+// 	);
+// };
+// ce(
+// 	'div',
+// 	{ id: 'my-first-component' },
+// 	ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+// 	ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+// 	ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+// 	ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+// );
+
+// render(<MyFirstComponent />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
 	context: __dirname,
-	entry: "./js/ClientApp.jsx",
-	devtool: "cheap-eval-source-map",
+	entry: ['./js/ClientApp.jsx'],
+	devtool: 'cheap-eval-source-map',
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'bundle.js'
@@ -20,8 +20,11 @@ module.exports = {
 		rules: [
 			{
 				test: /\.jsx?$/,
-				loader: 'babel-loader'
+				loader: 'babel-loader',
+				query: {
+					presets: ['react']
+				}
 			}
 		]
 	}
-}
+};
